0e71f3c3a9e324c236bb87d90e778003cbf4ecb1
Nuwa-Hub <nuwan@fcodelabs.com>
2023-09-29 09:22:51 +0530

    add

--- src/services/OpenAiService.ts
+++ src/services/OpenAiService.ts
@@ -1,9 +1,9 @@
 import { Configuration, OpenAIApi } from "openai";
 
-console.log(process.env.OPENAI_API_KEY);
+
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: "test",
 });
 
 const openai = new OpenAIApi(configuration);
